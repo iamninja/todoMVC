7467c2604dd6b3dfb6195d181fa0d65a221ec9eb
Vagios Vlachos <bagios234@gmail.com>
2014-04-14 12:23:50 +0300

    Display active/completed/all todos

--- js/router.js
+++ js/router.js
@@ -1,5 +1,9 @@
 Todos.Router.map(function() {
-	this.resource('todos', { path: '/' });
+	this.resource('todos', { path: '/' }, function() {
+		// Add child routes
+		this.route('active');
+		this.route('completed');
+	});
 });
 
 // Create todos class with 
@@ -7,5 +11,33 @@ Todos.Router.map(function() {
 Todos.TodosRoute = Ember.Route.extend({
 	model: function () {
 		return this.store.find('todo');
+	}
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+	model: function() {
+		return this.modelFor('todos');
+	}
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+	model: function(){
+		return this.store.filter('todo', function(todo) {
+			return !todo.get('isCompleted');
+		});
+	},
+	renderTemplate: function(controller) {
+		this.render('todos/index', {controller: controller});
+	}
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.filter('todo', function(todo) {
+			return todo.get('isCompleted');
+		});
+	},
+	renderTemplate: function(controller) {
+		this.render('todos/index', {controller: controller});
 	}
 });
